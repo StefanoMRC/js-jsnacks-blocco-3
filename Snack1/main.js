f1b805981e0717b0ba8150f5f1b40e2d9dfe99f8
StefanoMRC <stefano.mrc92@gmail.com>
2022-03-17 13:50:08 +0100

    fine primo esercizio

---
+++ Snack1/main.js
@@ -0,0 +1,15 @@
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è minore di 50.
+let array=[]
+let somma=0
+
+while (somma<50){
+    let num=parseInt(prompt("inserisci un numero"))
+    array.push(num)
+    somma=somma+num
+    
+};
+document.getElementById("contArray").innerHTML=array
+document.getElementById("contSomma").innerHTML=somma
+
+
+
